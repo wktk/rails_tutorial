d1e9ef3a5213a2f78c231d27bd4ad20f34542429
wktk <wktk30@gmail.com>
2015-10-09 18:05:23 +0900

    Check format validity of User.email

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
   validates :name, presence: true, length: { maximum: 50 }
-  validates :email, presence: true
+  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }
 end
