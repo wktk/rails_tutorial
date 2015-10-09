5e7b96412bc27bc9a30d003dd2f9055655a0a9de
wktk <wktk30@gmail.com>
2015-10-09 18:40:58 +0900

    Add has_secure_password to User

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,6 @@ class User < ActiveRecord::Base
   validates :name, presence: true, length: { maximum: 50 }
   validates :email, presence: true, format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
+
+  has_secure_password
 end
