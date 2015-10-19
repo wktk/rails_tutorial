1ff0e1cb46e193eab0fb7f7f252673ea2dbbe236
wktk <wktk30@gmail.com>
2015-10-19 14:08:17 +0900

    Finish 8.2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,11 +10,16 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      sign_in(@user)
       flash[:success] = "Welcome to the Sample App!"
       redirect_to @user
     else
       render 'new'
     end
+  end
+
+  def edit
+    @user = User.find(params[:id])
   end
 
 private
