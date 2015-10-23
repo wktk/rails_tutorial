a01c2e406a26dfdf05057524db55d099b92ab681
k.wakitani <wktk30@gmail.com>
2015-10-23 16:56:17 +0900

    Finish 10.3.1

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 RailsTutorial::Application.routes.draw do
   resources :users
   resources :sessions, only: [:new, :create, :destroy]
+  resources :microposts, only: [:create, :destroy]
   root  'static_pages#home'
   match '/signin',  to: 'sessions#new',         via: 'get'
   match '/signout', to: 'sessions#destroy',     via: 'delete'
