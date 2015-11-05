9d003cf00ebdc7b728825eec47539addfe487abd
k.wakitani <k.wakitani@gmail.com>
2015-11-05 11:19:15 +0900

    Finish 10.3.3

--- app/models/user.rb
+++ app/models/user.rb
@@ -18,6 +18,11 @@ class User < ActiveRecord::Base
     Digest::SHA1.hexdigest(token.to_s)
   end
 
+  def feed
+    # TODO
+    Micropost.where("user_id = ?", id)
+  end
+
 private
 
   def create_remember_token
